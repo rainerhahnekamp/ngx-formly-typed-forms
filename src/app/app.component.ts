921c703f994db2c677e50fefd825d124988c7de5
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-09-28 00:40:06 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { FormBuilder } from '@angular/forms';
+
+export type Address = {
+  id: number;
+  street: string;
+  streetNumber: string;
+  zip: string;
+  city: string;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngx-formly-typed-forms';
+  form = inject(FormBuilder).nonNullable.group<Address>({
+    id: 0,
+    street: '',
+    streetNumber: '',
+    zip: '',
+    city: '',
+  });
+
+  address: Address = {
+    id: 1,
+    street: 'Domgasse',
+    streetNumber: '5',
+    zip: '1010',
+    city: 'Vienna',
+  };
+
+  fields: FormlyFieldConfig[] = [
+    { key: 'id', type: 'input' },
+    { key: 'street', type: 'input' },
+    { key: 'streetNumber', type: 'input' },
+    { key: 'zip', type: 'input' },
+    { key: 'city', type: 'input' },
+  ];
+
+  handleSubmit() {
+    console.log(this.form.getRawValue());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormlyModule.forRoot(),
+    FormlyMaterialModule,
+    ReactiveFormsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
